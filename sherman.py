6cca55301d6aa047979ed0b47b756df2209f4068
김연철 <giao571@protonmail.com>
2014-03-03 15:20:37 +0800

    SAK-26268 - Mexican Spanish translation not working properly in Sakai 10

--- sherman.py
+++ sherman.py
@@ -6,3 +6,9 @@ class Matrix:
 
     def __init__(self, row: int, column: int, default_value: float = 0):
         """
+        <method Matrix.__init__>
+        Initialize matrix with given size and default value.
+
+        Example:
+        >>> a = Matrix(2, 3, 1)
+        >>> a
